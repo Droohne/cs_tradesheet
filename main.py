d43b967487d7d5974c81429866cd4fcae50f300b
Droohne <ne4toctrannoe@yandex.ru>
2024-08-22 18:14:10 +0500

    Too efficient algo reuslted in a ban. Iterate over id instead

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from os.path import getsize
 from requests import get
 from json import dumps, loads
 from pathlib import Path
+import time
+import decimal
 
 
 # TODO if using API you can only get lowest buy order price (from skins to cash)
@@ -26,9 +30,7 @@ def convert_CNY_RUB():
     # hide it later
     # API_KEY = fc58b1da2479d5493a749550 # i dont need id xd
     req_json = get("https://api.exchangerate-api.com/v4/latest/CNY").json()
-    CNY_TO_RUB = req_json["rates"][
-        "RUB"
-    ]  # if steam will continue prices in USD change it to USD
+    CNY_TO_RUB = req_json["rates"]["RUB"]
     return CNY_TO_RUB
 
 
@@ -38,37 +40,23 @@ def show(items):
         print(keys, values)
 
 
-def next_page(
-    driver, page_number
-):  # Может быть просто по ссылке переходить, вместо нажатия кнопки next page?
-    # на 7 странице XPATH кнопки next page меняется на /html/body/div[5]/div[1]/div[4]/div[2]/ul/li[13]/a
-    # с 8 XPATH становится таким /html/body/div[5]/div[1]/div[4]/div[2]/ul/li[14]/a и остается таким
-    xpath = ""
-    if page_number < 6:
-        xpath = "/html/body/div[5]/div[1]/div[4]/div[2]/ul/li[12]/a"
-    elif page_number == 6:
-        xpath = "/html/body/div[5]/div[1]/div[4]/div[2]/ul/li[13]/a"
-    else:
-        xpath = "/html/body/div[5]/div[1]/div[4]/div[2]/ul/li[14]/a"
+def next_page(driver, amount_of_tries = 0):
     try:
-        v = driver.find_element(By.XPATH, xpath)
-        driver.find_element(By.XPATH, xpath).click()
-    except Exception as er:
-        if (
-            er.msg
-            == "Unable to locate element: /html/body/div[5]/div[1]/div[4]/div[2]/ul/li[14]/a; For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#no-such-element-exception"
-        ):
-            # Use global variable instead of wasted loops?
-            return
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.LINK_TEXT, "Next page"))).click()
+    except:
+        time.sleep(0.1)
+        if amount_of_tries > 1:
+            driver.get(driver.current_ulr)
+        next_page(driver, amount_of_tries + 1)
 
 
 def find(driver, element_XPATH):
     try:
-        v = driver.find_element(By.XPATH, element_XPATH).text
+        v = driver.find_element(By.XPATH, element_XPATH).text 
         if v is not None:
             return v
     except:
-        driver.implicitly_wait(1)
+        pass
     return find(driver, element_XPATH)
 
 
@@ -83,61 +71,72 @@ def setup():
     #opts.add_argument("--headless")
     serv = webdriver.FirefoxService(executable_path="/snap/bin/geckodriver")
     driver = webdriver.Firefox(options=opts, service=serv)
-
-    # logging in by hand (for now)
-    steam_login_link = "https://steamcommunity.com/login/home/"
+    driver
+    # logging in by hand (for now). Make a throw away aсcount without 2FA and use its data (login/pass) in env var if coockies not valid
+    steam_login_link = "https://steamcommunity.com/login/home/"  
     driver.get(steam_login_link)
-    for cookie in loads(Path("cookies.json").read_text()):
-        driver.add_cookie(cookie)
-    driver.refresh()
-    driver.implicitly_wait(3)
-    login_link = "https://steamcommunity.com/openid/login?openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.realm=https%3A%2F%2Fbuff.163.com%2F&openid.sreg.required=nickname%2Cemail%2Cfullname&openid.assoc_handle=None&openid.return_to=https%3A%2F%2Fbuff.163.com%2Faccount%2Flogin%2Fsteam%2Fverification%3Fback_url%3D%252Faccount%252Fsteam_bind%252Ffinish&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select"
-    driver.get(login_link)
-    driver.find_element(By.XPATH, '//*[@id="imageLogin"]').click()
     # Manual generation of cookies for further automatization
     # Get cookies to a json file:
     # generate_cookies(driver)
     # driver.close()
+    for cookie in loads(Path("cookies.json").read_text()):
+        driver.add_cookie(cookie)
+    driver.refresh()
+    driver.implicitly_wait(5)
+    login_link = "https://steamcommunity.com/openid/login?openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.realm=https%3A%2F%2Fbuff.163.com%2F&openid.sreg.required=nickname%2Cemail%2Cfullname&openid.assoc_handle=None&openid.return_to=https%3A%2F%2Fbuff.163.com%2Faccount%2Flogin%2Fsteam%2Fverification%3Fback_url%3D%252Faccount%252Fsteam_bind%252Ffinish&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select"
+    driver.get(login_link)
+    driver.find_element(By.XPATH, '//*[@id="imageLogin"]').click()
     return driver
-
 
 def save_items(items):
     open('file.txt', 'w').close() #for clearing file
     f = open("items.txt", "a")
     for item, value in items.items():
-        f.write(item + " | " + value + "\n")
+        f.write(item + " | " + str(value) + "\n")
     f.close()
 
 
-def get_items(number_of_pages):
+def get_links(weapon_names_list):
+    links = []
+    for name in weapon_names_list:
+        links.append(f"https://buff.163.com/market/csgo#game=csgo&page_num=1&category=weapon_{name}&tab=selling")
+    return links
+
+def get_items():
+    weapon_list = ["ak47","awp","m4a1","m4a1_silencer","famas","galilar","ssg08","usp_silencer","glock","geagle","mp9"]
+    full_st = time.time()
+    CNY_RUB_price = convert_CNY_RUB() * 1.02 # with site commision
     driver = setup()
-    link = "https://buff.163.com/market/csgo#game=csgo&page_num=1&category_group=rifle&tab=top-bookmarked"
-    driver.get(link)
+    links = get_links(weapon_list)
     XPATH_list = {}
-    items = {}
+    items = {} # until no db this will stay as it is. When db it will be imported from it
     for i in range(1, 21):
         # name of a weapon = price (in XPATH values)
         XPATH_list[f"html/body/div[5]/div[1]/div[4]/div[1]/ul/li[{i}]/h3/a"] = (
             f"/html/body/div[5]/div[1]/div[4]/div[1]/ul/li[{i}]/p/strong"
         )
 
-    for i in range(0, number_of_pages):
-        for xpath_name in XPATH_list:
-            t = find(driver, xpath_name)
-            if t not in items:
+    for link in links:
+        driver.get(link)
+        last_page_xpath = "/html/body/div[5]/div[1]/div[4]/div[2]/ul/li[13]/a"
+        n = int(find(driver, last_page_xpath))
+        time.sleep(0.001)
+        for i in range(1, n):
+            #st = time.time()
+            for xpath_name in XPATH_list:
+                t = find(driver, xpath_name)
                 watch = find(driver, XPATH_list[xpath_name])
-                items[t] = watch
-            else:  # As items come with stickers already installed, they doesnt matter to me as i want minimal price for given weapon (item) so does the float value
-                price = find(driver, XPATH_list[xpath_name])
-                b = float(price[2:])
-                c = float(items[t][2:])
-                if b < c:
-                    items[t] = price
-        next_page(driver, i)
+                items[t] = "{:.2f}".format(float(watch[2:]) * CNY_RUB_price)
+            next_page(driver)
+            #et = time.time()
+            #print("Elapsed time in seconds for 1 iteration = ", et-st )
     save_items(items)
     driver.close()
+    full_et = time.time()
+    full_time = full_et - full_st
+    print("full exec time is = " , full_time)
     return items
 
 
 if __name__ == "__main__":
-    get_items(25)
+    get_items()
