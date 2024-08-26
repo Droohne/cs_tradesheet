6ddc5f0ad8aed0801455f5c9867fba81b308c6af
Droohne <ne4toctrannoe@yandex.ru>
2024-08-26 14:18:48 +0500

    Added steam trade volume

--- json_manipulations.py
+++ json_manipulations.py
@@ -1,9 +1,9 @@
 import json
-
+import main
 def expression_parse_1(line):
-    want_items = ["AK-47", "MP9", "MAC-10", "SSG-08", "Galil AR", "FAMAS", "M4A1-S", "M4A4", "USP-S", "Glock-18", "Desert-Eagle", "Case"]
+    want_items = ["AK-47", "MP9", "MAC-10", "SSG 08", "Galil AR", "FAMAS", "M4A1-S", "M4A4", "USP-S", "Glock-18", "Desert-Eagle", "Case"]
     for item in want_items:
-        if item in line:
+        if item.lower() in line.lower():
             if (item == "Case" and "Case Hardened") or "Sticker" in line or "Souvenir" in line:
                 return False
             return True
@@ -34,10 +34,88 @@ def delete_youpin_id():
                 else:
                     file.write(line)  
 
+def steam_web_api_test():
+    #rewrite json file and delete useless info from it.
+    USD_to_RUB = main.convert_to_RUB("USD")
+    with open('prepared_json.json') as json_file:
+        #prices stored in USD
+        data = json.load(json_file)
+        items = {}
+        i = 0
+        for item in data:
+            for wear in data[item]:
+                new_item = {
+                "skinname": data[item][wear]['name'],
+                "skin_wear":  wear,
+                "pricelatest": round(data[item][wear]["pricelatest"]*USD_to_RUB,2),
+                "pricelatestsell": round(data[item][wear]["pricelatestsell"]*USD_to_RUB,2),
+                "priceupdatedat": data[item][wear]["priceupdatedat"],
+                "pricemin": round(data[item][wear]["pricemin"]*USD_to_RUB,2),
+                "pricereal": round(data[item][wear]["pricereal"]*USD_to_RUB,2),
+                "pricereal24h": round(data[item][wear]["pricereal24h"]*USD_to_RUB,2),
+                "offervolume": data[item][wear]["offervolume"],
+                "sold24h": data[item][wear]["sold24h"],
+                "sold7d": data[item][wear]["sold7d"],
+                "updatedat": data[item][wear]["updatedat"],
+                }          
+                if item in items:
+                    items[item][wear] = new_item
+                else:
+                    new_item = {
+                        wear: new_item
+                    }
+                    items[item] = new_item
+    return items
+
+def prepare_steam_web_api():
+    #yes you can directly ask for a given type of weapon but cmon, it isnt for us free plesbs (500 requests per month)
+    with open('steam_api.json', 'r') as json_file:
+        data = json.load(json_file)
+        i=0
+        items = {}
+        for item in data:
+            i+=1
+            if item['itemtype'] is not None:
+                if expression_parse_1(item['itemtype']):
+                    name = item['itemtype'] + " | " + item["itemname"]
+                    wear = item['wear']
+                    only_required_fields = {
+                        "name" : name,
+                        'wear' : wear,
+                        "pricelatest" : item['pricelatest'],
+                        'pricelatestsell' : item['pricelatestsell'],
+                        "priceupdatedat" : item['priceupdatedat'],
+                        "pricemin": item["pricemin"],
+                        'pricereal' : item['pricereal'],
+                        "pricereal24h" : item["pricereal24h"],
+                        "pricerealcreatedat" : item['pricerealcreatedat'],
+                        'pricemedian' : item['pricemedian'], 
+                        "priceavg" : item['priceavg'],
+                        'offervolume': item["offervolume"],
+                        'sold24h' : item['sold24h'],
+                        "sold7d" : item['sold7d'],
+                        'updatedat' : item['updatedat'],  
+                    }
+                    if name in items:
+                        items[name][wear] = only_required_fields
+                    else:
+                        new_item = {
+                            wear: only_required_fields
+                        }
+                        items[name] = new_item
+        return items
+    
+def write_json(dictionary, file_name):
+    #doesnt autoformat. Write a 1-liner
+    open(file_name, 'w').close()
+    with open(file_name, 'a') as data_json:
+        json.dump(dictionary, data_json)
+            
 
 if __name__ == "__main__":
     #write_only_needed()
     #delete_youpin_id()
-    #After this i deleted inner {} (not contents inside of it) using ctrl + H.
-    #Must be done by hand. Probably can be automated...
-    pass
+    #After this i deleted inner {} (not contents inside of it) using ctrl + H. #Must be done by hand.
+    #Download file into steam_api_test.json first
+    write_json(prepare_steam_web_api(), "prepared_json.json")
+    write_json(steam_web_api_test(), "steam_result.json")
--- main.py
+++ main.py
@@ -2,79 +2,114 @@ import requests
 import json
 import time
 import random
-
+import json_manipulations
+from os import environ
 # TODO if using API you can only get lowest buy order price (from skins to cash)
 # see example https://buff.163.com/api/market/goods/buy_order?game=csgo&goods_id=34306
 # TODO use this instead https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=33811
 # TODO see in network page (GET - requests) try to ask server to get items instead of using selenium
 
 
-
-def convert_CNY_RUB():
-    req_json = requests.get("https://api.exchangerate-api.com/v4/latest/CNY").json()
-    CNY_TO_RUB = req_json["rates"]["RUB"]
-    return CNY_TO_RUB
-
-def save_items(items):
-    with open("sample.json", "w") as outfile: 
-        json.dump(items, outfile)
+def convert_to_RUB(currency_acronym_name):
+    req_json = requests.get(f"https://api.exchangerate-api.com/v4/latest/{currency_acronym_name}").json()
+    in_rub = req_json["rates"]["RUB"]
+    return in_rub
 
 
-def get_items(number_of_items):
-    CNY_RUB_price = convert_CNY_RUB() #without commision
+def get_items_steam_API(API_key):
+    req_json = requests.get(f"https://www.steamwebapi.com/steam/api/items?key={API_key}").json()
+    json_manipulations.write_json(req_json, "steam_api.json")
+
+
+def get_additional_data(item, wear):
+    with open("steam_result.json", "r") as steam_json:
+        data = json.load(steam_json)
+        try:
+            return data[item.lower()][wear]
+        except:
+            empty_data = {
+                "skinname": None,
+                "skin_wear": None,
+                "pricelatest": None,
+                "pricelatestsell": None,
+                "priceupdatedat": None,
+                "pricemin": None,
+                "pricereal": None,
+                "pricereal24h": None,
+                "offervolume": None,
+                "sold24h": None,
+                "sold7d": None,
+                "updatedat": None,
+            }
+            return empty_data
+
+
+def get_wear_acronym(wear):
+    line = ""
+    if wear.lower() == "factory new":
+        line = "fn"
+    elif wear.lower() == "battle-scarred":
+        line = "bs"
+    elif wear.lower() == "minimal wear":
+        line = "mw"
+    elif wear.lower() == "field-tested":
+        line = "ft"
+    else:
+        line = "ww"
+    return line
+
+
+def get_items():
+    CNY_RUB_price = convert_to_RUB("CNY")  # without commision
     base_string = "https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id="
     item_id = 1
     dataset = {}
     with open("fixed_items.json") as jf:
-        data = json.load(jf)
-        quality_json_template = {
-            "buff163_id": None,
-            "price": None,
-            "steam_price": None,
-            "steam_trade_volume": None,
-            "buff163_amount_of_sell_offers": None,
-            "buff163_amount_of_buy_offers": None
-        }
-        for name in data:            
-            time.sleep(random.random() + 0.5) # without this at a 100 or so ids will get To Many Requests
+        data = json.load(jf)  # 2690 items in there
+        for name in data:
+            time.sleep(random.random() + 0.5)  
+            # without this at a 100 or so ids will get To Many Requests
             id = data[name]
-            s = base_string+f"{id}"
+            s = base_string + f"{id}"
+            #extracting info from API on each and every weapon skin
             req = requests.get(s)
             req_weapon = req.json()
             if len(req_weapon["data"]["goods_infos"]) > 0:
-                name_quality = req_weapon["data"]["goods_infos"][f"{id}"]["market_hash_name"] # this should give eng name
-                value = round(float(req_weapon["data"]["items"][0]["price"])*CNY_RUB_price, 2)
-                skin_name = name_quality.split(' (')[0]
-                quality = name_quality.split(' (')[1][:-1]
-                steam_price = float(req_weapon["data"]["goods_infos"][f"{id}"]["steam_price_cny"])*CNY_RUB_price
+                name_wear = req_weapon["data"]["goods_infos"][f"{id}"]["market_hash_name"]
+                value = round(float(req_weapon["data"]["items"][0]["price"]) * CNY_RUB_price, 2)
+                skin_name = name_wear.split(" (")[0] # wrong result on M4A4 | 龍王 (Dragon King) 
+                wear = get_wear_acronym(name_wear.split(" (")[1][:-1])
+                steam_price = float(req_weapon["data"]["goods_infos"][f"{id}"]["steam_price_cny"]) * CNY_RUB_price
             else:
                 continue
-            quality_json = {}
-            quality_json["price"] = value
-            quality_json["steam_price"] = round(steam_price, 2)
-            quality_json["buff163_id"] = id
-            #other field will be None till i find a way to fill them (or delete them)
+            
+            # "Helper" json
+            wear_json = {}
+            wear_json["price"] = value
+            wear_json["steam_price"] = round(steam_price, 2)
+            wear_json["buff163_id"] = id
+
+            additional_data = get_additional_data(skin_name, wear)
+
+            for item in additional_data:
+                wear_json[item] = additional_data[item]
 
             if skin_name in dataset:
-                dataset[skin_name][quality] = quality_json
+                dataset[skin_name][wear] = wear_json
             else:
 
-                json_item = {
-                    "id" : item_id,
-                    quality: quality_json
-                }
+                json_item = {"id": item_id, wear: wear_json}
                 dataset[skin_name] = json_item
-            
-            item_id+=1
-            if item_id > number_of_items:
-                break
-    save_items(dataset)
+            item_id += 1
+            print(item_id)
+    return dataset
+
 
 if __name__ == "__main__":
+    # key = environ("steam_web_api_key")
+    # get_items_steam_API(key)
     full_st = time.time()
-    get_items(500)
+    json_manipulations.write_json(get_items(), "result.json")
     full_et = time.time()
     full_time = full_et - full_st
     print("full exec time is = ", full_time)
-
-
